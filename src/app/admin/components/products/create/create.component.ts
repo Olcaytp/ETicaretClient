93f60c151e7b8edecb7dd7392a38394b1a09fca0
olcay <olcaytapan10@gmail.com>
2023-04-13 16:08:56 +0200

    custom-validation-filter-added-20

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
@@ -19,12 +19,32 @@ export class CreateComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  @Output() createdProduct: EventEmitter<Create_Product> = new EventEmitter();
+
   create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
     this.showSpinner(SpinnerType.SquareLoader);
     const create_product: Create_Product = new Create_Product();
     create_product.name = name.value;
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
+
+    if(!name.value) {
+      this.alertify.message("Name is required", {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      });
+      return;
+    }
+
+    if(parseInt(stock.value)) {
+      this.alertify.message("Stock is required", {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      });
+      return;
+    }
 
     this.productService.createProduct(create_product, () => {
       this.hideSpinner(SpinnerType.SquareLoader);
@@ -33,6 +53,14 @@ export class CreateComponent extends BaseComponent implements OnInit {
         messageType: MessageType.Success,
         position: Position.TopRight
       });
+      this.createdProduct.emit(create_product);
+    }, errorMessage => {
+      this.alertify.message(errorMessage,
+        {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        });
     });
   }
 
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { Create_Product } from '../../../contracts/create_product';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,21 @@ export class ProductService {
 
   constructor(private HttpClientService: HttpClientService) { }
 
-  createProduct(product: Create_Product, successCallback?: any) {
+  createProduct(product: Create_Product, successCallback?: () => void, errorCallBack?: (errorMessage: string) => void) {
     this.HttpClientService.post({
       controller: "products"
     }, product).subscribe((response) => {
       successCallback();
-    })
+    }, (errorResponse: HttpErrorResponse) => {
+      const _error: Array<{ key: string, value: Array<string> }> = errorResponse.error;
+      let message = "";
+      _error.forEach((v, index) => {
+        v.value.forEach((_v, _index) => {
+          message += `${_v}<br>`;
+        });
+      });
+      errorCallBack(message);
+    });
   }
+
 }
