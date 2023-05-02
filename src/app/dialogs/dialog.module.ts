e9deda1789edecd063d3ce290a8e2bc1ccc1d1fe
olcay <olcaytapan10@gmail.com>
2023-05-03 00:07:29 +0200

    product-images-add-33

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
-import { FileUploadDialogComponent } from './file-upload-dialog/file-upload-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { SelectProductImageDialogComponent } from './select-product-image-dialog/select-product-image-dialog.component';
 import { FileUploadModule } from '../services/common/file-upload/file-upload.module';
+
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -16,7 +17,7 @@ import { FileUploadModule } from '../services/common/file-upload/file-upload.mod
   ],
   imports: [
     CommonModule,
-    MatDialogModule, MatButtonModule,
+    MatDialogModule, MatButtonModule, MatCardModule,
     FileUploadModule
   ]
 })
