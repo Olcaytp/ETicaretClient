44f9e8c7c7c54721b9c7b6375be31c2ebef14b7f
olcay <olcaytapan10@gmail.com>
2023-05-04 20:07:10 +0200

    asp.netcore-identity-mechanism-created-38

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Create_User } from 'src/app/contracts/users/create_user';
+import { User } from 'src/app/entities/user';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
+import { UserService } from 'src/app/services/common/models/user.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +17,8 @@ export class RegisterComponent extends BaseComponent implements OnInit {
 
   constructor(spinner: NgxSpinnerService,
     private httpClientService: HttpClientService,
+    private userService: UserService,
+    private toastrService: CustomToastrService,
     private formBuilder: FormBuilder
     ) {
     super(spinner);
@@ -62,14 +68,24 @@ export class RegisterComponent extends BaseComponent implements OnInit {
   }
 
   submitted: boolean = false;
-  onSubmit(data: any) {
+  async onSubmit(user: User) {
     this.submitted = true;
 
-    var x = this.component;
-    debugger;
-
-    if (this.frm.invalid) {
+    if (this.frm.invalid)
       return;
+
+    const result: Create_User =  await this.userService.create(user);
+    if(result.succeeded) {
+      this.toastrService.message(result.message, "Kayıt Başarılı", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      });
+    }
+    else {
+      this.toastrService.message(result.message, "Kayıt Başarısız", {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight
+      });
     }
   }
 
