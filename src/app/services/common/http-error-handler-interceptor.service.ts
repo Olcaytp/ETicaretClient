8f6d76145a5759d12fe3334ea7926739a2079dfa
olcay <olcaytapan10@gmail.com>
2023-05-07 15:30:11 +0200

    httpInterceptor-globalError-Handler-46

---
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -0,0 +1,57 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpStatusCode } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Observable, catchError, of } from 'rxjs';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../ui/custom-toastr.service';
+import { UserAuthService } from './models/user-auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpErrorHandlerInterceptorService implements HttpInterceptor {
+
+  constructor(private toastrService: CustomToastrService,
+              private userAuthService: UserAuthService,
+              private router: Router,
+              private spinner: NgxSpinnerService
+              ) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(error => {
+      switch(error.status){
+        case HttpStatusCode.Unauthorized:
+          this.toastrService.message("Yetkisiz işlem", "Yetkisiz işlem", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.InternalServerError:
+          this.toastrService.message("Sunucu hatası", "Sunucu hatası", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.BadRequest:
+          this.toastrService.message("Hatalı istek", "Hatalı istek", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        case HttpStatusCode.NotFound:
+          this.toastrService.message("İstek bulunamadı", "İstek bulunamadı", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+        default:
+          this.toastrService.message("Bilinmeyen hata", "Bilinmeyen hata", {
+            messageType: ToastrMessageType.Warning,
+            position: ToastrPosition.BottomFullWidth
+          });
+          break;
+      }
+      return of(error);
+    }));
+  }
+}
